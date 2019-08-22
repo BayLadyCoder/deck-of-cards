a9592050f7d6aafb41f79efbd79af111215cacfc
BayLadyCoder <rachada_32@hotmail.com>
2019-08-21 23:58:54 -0400

    show all cards that been fetched

---
+++ src/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Card(props) {
+  return <img src={props.img} alt={props.name} />;
+}
+
+export default Card;
--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./DeckOfCards.css";
 import axios from "axios";
+import Card from "./Card";
 
 export class DeckOfCards extends Component {
   constructor(props) {
     super(props);
     this.state = {
       deck_id: "",
-      current_card: "",
-      card_img: "",
+      showed_cards: [],
       remaining: 52
     };
     this.getRandomCard = this.getRandomCard.bind(this);
@@ -27,25 +27,34 @@ export class DeckOfCards extends Component {
     const url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/`;
     const res = await axios.get(url);
     const data = res.data;
-    console.log(data);
+    // console.log(data);
     const value = data.cards[0].value;
     const suit = data.cards[0].suit;
-    const current_card = `${value} ${suit}`;
+    const card_name = `${value} ${suit}`;
     const card_img = data.cards[0].image;
     const remaining = data.remaining;
+    let cards = [];
+    cards.push({
+      card_name: card_name,
+      card_img: card_img
+    });
+
     this.setState({
-      current_card: current_card,
-      card_img: card_img,
+      showed_cards: [...this.state.showed_cards, ...cards],
       remaining: remaining
     });
   }
 
   render() {
+    const cards = this.state.showed_cards.map(c => (
+      <Card img={c.card_img} name={c.card_name} />
+    ));
+    // console.log("State", this.state);
     return (
       <div className="DeckOfCards">
         <h1>Deck Of Cards</h1>
         <button onClick={this.getRandomCard}>Click Me</button>
-        <img src={this.state.card_img} alt={this.state.current_card} />
+        <div className="cards">{cards}</div>
       </div>
     );
   }
