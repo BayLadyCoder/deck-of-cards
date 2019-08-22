747c3ddccb8dbbe5d94f4a1e9447987c8bb00418
BayLadyCoder <rachada_32@hotmail.com>
2019-08-22 00:40:30 -0400

    Updates

---
+++ precache-manifest.8e6da236c868b1baa7de4e8db25ade39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef9d4556801f1e35edfa6772fd90cf3",
+    "url": "/deck-of-cards/index.html"
+  },
+  {
+    "revision": "42c9eb12d641829543c2",
+    "url": "/deck-of-cards/static/css/main.ee2449d8.chunk.css"
+  },
+  {
+    "revision": "8625e527bf9b35802a67",
+    "url": "/deck-of-cards/static/js/2.1badf9e5.chunk.js"
+  },
+  {
+    "revision": "42c9eb12d641829543c2",
+    "url": "/deck-of-cards/static/js/main.a4603d2a.chunk.js"
+  },
+  {
+    "revision": "5655dc6f312071826e64",
+    "url": "/deck-of-cards/static/js/runtime~main.467f6679.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deck-of-cards/static/media/logo.5d5d9eef.svg"
+  }
+]);
